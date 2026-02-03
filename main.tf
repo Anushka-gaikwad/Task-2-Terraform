0747d090acc8b00ff71dbccb6022112709b0fcdb
Anushka-gaikwad <anushkagaikwad0111@gmail.com>
2026-02-03 11:33:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_instance" "my-ec2" {
     subnet_id = aws_subnet.public.id
     vpc_security_group_ids = aws_security_group.sg.id
     tags = {
-        Name = "teraaform-ec2"
+        Name = "my-terraform-ec2"
     }
     associate_public_ip_address = true
 }
