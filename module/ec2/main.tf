31a76bb98dfc23af71804795de03eb22a0d42fa8
Anushka-gaikwad <anushkagaikwad0111@gmail.com>
2026-02-04 11:05:31 +0530

    Update main.tf

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -1,12 +1,11 @@
-resource "aws_instance" "devops" {
+resource "aws_instance" "terraform-ec2" {
     ami = "ami-068c0051b15cdb816"
     instance_type = "t2.micro"
-    key_name  = "N.verginia-key"
     subnet_id = aws_subnet.public.id
-    vpc_security_group_ids = aws_security_group.sg.id
-    key_name                    = aws_key_pair.ec2_key_pair.key_name
+    vpc_security_group_ids = [aws_security_group.sg.id]
+    key_name  = aws_key_pair.ec2_key_pair.key_name
     tags = {
-        Name = "auto-ec2"
+        Name = "my-ec2"
     }
     associate_public_ip_address = true
 }
