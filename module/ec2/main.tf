4760684e7d48dfebf08b6cc52a094f8683ce566d
Anushka-gaikwad <anushkagaikwad0111@gmail.com>
2026-02-04 11:09:26 +0530

    Update main.tf

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform-ec2" {
     ami = "ami-068c0051b15cdb816"
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
     subnet_id = aws_subnet.public.id
     vpc_security_group_ids = [aws_security_group.sg.id]
     key_name  = aws_key_pair.ec2_key_pair.key_name
